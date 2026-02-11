6b390d7f9330bdf6ee9c188d07283235468f12c8
Thalles Kaian <contato.thalles@outlook.com>
2026-02-11 08:02:13 -0300

    Tratamento inicial de erros

--- pokedex/js/scripts.js
+++ pokedex/js/scripts.js
@@ -14,25 +14,35 @@ const fetchPokemon = async (pokemon) => {
 
     const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
-// Tem de pegar o resultado da requisição, e transformar em um formato que o JavaScript consiga ler, que é o JSON. O método .json() é usado para isso, e ele também retorna uma Promise, porque a conversão pode levar algum tempo. Por isso, usamos await para esperar a conclusão dessa operação antes de continuar com o código. 
-    const pokemonData = await apiResponse.json()
+    if(apiResponse.status == 200) {
 
-    return pokemonData
+        // Tem de pegar o resultado da requisição, e transformar em um formato que o JavaScript consiga ler, que é o JSON. O método .json() é usado para isso, e ele também retorna uma Promise, porque a conversão pode levar algum tempo. Por isso, usamos await para esperar a conclusão dessa operação antes de continuar com o código. 
+
+        const pokemonData = await apiResponse.json()
+        return pokemonData
+    }
 }
 
 const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon)
     
-    pokemonName.innerHTML = data.name
-    
-    pokemonNumber.innerHTML = data.id
+    if(data) {
+        pokemonName.innerHTML = data.name
+        pokemonNumber.innerHTML = data.id + " - "
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        // Limpa o campo de input após a busca.
+        searchInput.value = ''
+    } else {
+        alert("Pokémon não encontrado!")
+        searchInput.value = ''
+        pokemonName.innerHTML = 'Não encontrado'
+        pokemonNumber.innerHTML = ''
+        pokemonImage.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'
+    }
 
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 
     
-    // Limpa o campo de input após a busca.
-    searchInput.value = ''
 
 }
 
